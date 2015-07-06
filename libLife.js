69cb5102c19066995dd9ea6e151733217979b8c2
olejekg <OlejekG@ukr.net>
2015-07-07 02:35:18 +0300

    The simplified library.
    The library was easier byt if cell near a border, it works not correct.

--- libLife.js
+++ libLife.js
@@ -1,4 +1,5 @@
-        var lifeLib = {}
+var i,j; 
+var lifeLib = {}
         
         lifeLib.grida = function(xMax, yMax) { //Draw a grid:
             for (y = 0; y <= yMax; y += cellSize) { //Horizontal lines;
@@ -15,158 +16,17 @@
         lifeLib.countLive = function(x, y) { //Count of living neighbors:
             var countL = 0;
             if (x && y && x < (cellX - 1) && y < (cellY - 1)) { //the cell is in the middle weeding (not on the edge);
-                if (cell[x - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[x + 1][y - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[x - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[x + 1][y + 1])
-                    countL++;
-            } else if (!x && y && y < (cellY - 1)) { //The cell is on the left border;
-                if (cell[cellX - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[x + 1][y - 1])
-                    countL++;
-                if (cell[cellX - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[cellX - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[x + 1][y + 1])
-                    countL++;
-            } else if (x == (cellX - 1) && y && y < (cellY - 1)) { //The cell is on the right border;
-                if (cell[x - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[0][y - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[0][y])
-                    countL++;
-                if (cell[x - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[0][y + 1])
-                    countL++;
-            } else if (x && x < (cellX - 1) && !y) { //The cell is on the top;
-                if (cell[x - 1][cellY - 1])
-                    countL++;
-                if (cell[x][cellY - 1])
-                    countL++;
-                if (cell[x + 1][cellY - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[x - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[x + 1][y + 1])
-                    countL++;
-            } else if (x && x < (cellX - 1) && y == (cellY - 1)) { //The cell is in the bottom;
-                if (cell[x - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[x + 1][y - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[x - 1][0])
-                    countL++;
-                if (cell[x][0])
-                    countL++;
-                if (cell[x + 1][0])
-                    countL++;
-            } else if (!x && !y) { //The cell is in the upper left corner;
-                if (cell[cellX - 1][cellY - 1])
-                    countL++;
-                if (cell[x][cellY - 1])
-                    countL++;
-                if (cell[x + 1][cellY - 1])
-                    countL++;
-                if (cell[cellX - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[cellX - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[x + 1][y + 1])
-                    countL++;
-            } else if (x == (cellX - 1) && !y) { //The cell is in the upper right corner;
-                if (cell[x - 1][cellY - 1])
-                    countL++;
-                if (cell[x][cellY - 1])
-                    countL++;
-                if (cell[0][cellY - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[0][y])
-                    countL++;
-                if (cell[x - 1][y + 1])
-                    countL++;
-                if (cell[x][y + 1])
-                    countL++;
-                if (cell[0][y + 1])
-                    countL++;
-            } else if (x == (cellX - 1) && y == (cellY - 1)) { //The cell is in the lower right corner;
-                if (cell[x - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[0][y - 1])
-                    countL++;
-                if (cell[x - 1][y])
-                    countL++;
-                if (cell[0][y])
-                    countL++;
-                if (cell[x - 1][0])
-                    countL++;
-                if (cell[x][0])
-                    countL++;
-                if (cell[0][0])
-                    countL++;
-            } else if (!x && y == (cellY - 1)) { //The cell is in the left lower corner;
-                if (cell[cellX - 1][y - 1])
-                    countL++;
-                if (cell[x][y - 1])
-                    countL++;
-                if (cell[x + 1][y - 1])
-                    countL++;
-                if (cell[cellX - 1][y])
-                    countL++;
-                if (cell[x + 1][y])
-                    countL++;
-                if (cell[cellX - 1][0])
-                    countL++;
-                if (cell[x][0])
-                    countL++;
-                if (cell[x + 1][0])
-                    countL++;
-            }
+
+for (i=(x-1);i<=(x+1);i++) {
+for (j=(y-1);j<=(y+1);j++) {
+if (cell[i][j])
+
+countL++;
+}
+}
+if (cell[x][y])
+countL--;
+}
+
             return countL; //Return number of living neighbors as result of function.
         }
